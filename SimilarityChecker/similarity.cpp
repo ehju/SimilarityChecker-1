f8690b02d20e396a31048e4b51ce7c64fe27acab
ehju <eh13.ju@samsung.com>
2025-07-23 16:23:04 +0900

    [refactoring] Similarity code refactoring

--- SimilarityChecker/similarity.cpp
+++ SimilarityChecker/similarity.cpp
@@ -1,37 +1,33 @@
 #include <string>
-using std::string;
+using namespace std;
 
 class Similarity {
 public:
 	int getLengthScore(const string& str1, const string& str2) {
-		if (isSameLength(str1, str2)) return 60;
-		if (isOverTwiceSize(str1, str2))
-			return 0;
-		if (str1.length() >= str2.length()) {
-			double gap = str1.length() - str2.length();
-			//calculation
-			return (1 - (gap / str2.length())) * 60;
-		}
-		else {
-			double gap = str2.length() - str1.length();
-			//calculation
-			return (1 - (gap / str1.length())) * 60;
-		}
+
+		int longer = max(str1.length(), str2.length());
+		int shorter = min(str1.length(), str2.length());
+		if (isSameLength(longer, shorter)) return 60;
+		else if (isOverTwiceSize(longer, shorter)) return 0;
+		else return calculatePartialpoints(longer, shorter);
 	}
-	bool isOverTwiceSize(const std::string& str1, const std::string& str2)
+private:
+	int calculatePartialpoints(int longer,int shorter)
 	{
-		if (str1.length() >= str2.length()) {
-			if (str1.length() >= (str2.length() * 2))
-				return true;
-		}
-		else {
-			if (str2.length() >= (str1.length() * 2))
-				return true;
-		}
+		double gap;
+		gap = longer - shorter;
+		return (1 - (gap / shorter)) * 60;
+	}
+	bool isOverTwiceSize(int longer, int shorter)
+	{
+
+		if (longer >= (shorter * 2))
+			return true;
 		return false;
+
 	}
-	bool isSameLength(const std::string& str1, const std::string& str2)
+	bool isSameLength(int longer, int shorter)
 	{
-		return str1.length() == str2.length();
+		return longer==shorter;
 	}
 };
