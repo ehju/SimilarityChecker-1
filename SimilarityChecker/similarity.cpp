6b57628951363b7219e7f47f4869ac57360eb88a
ehju <eh13.ju@samsung.com>
2025-07-23 15:25:06 +0900

    [refactoring] add SameSizeTest - change variable name

--- SimilarityChecker/similarity.cpp
+++ SimilarityChecker/similarity.cpp
@@ -3,8 +3,8 @@ using std::string;
 
 class Similarity {
 public:
-	int getLengthScore(const string& a, const string& b) {
-		if (a==b)
-			return 60;
+	int getLengthScore(const string& str1, const string& str2) {
+		if (str1 == str2) return 60;
+		else return 0;
 	}
 };
