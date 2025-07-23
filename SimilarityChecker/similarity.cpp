c14d912f70c4880d2f9f596a52f464f9cfee312a
ehju <eh13.ju@samsung.com>
2025-07-23 15:43:03 +0900

    [refactoring] OverTwiceSize Test - refactoring

--- SimilarityChecker/similarity.cpp
+++ SimilarityChecker/similarity.cpp
@@ -4,19 +4,25 @@ using std::string;
 class Similarity {
 public:
 	int getLengthScore(const string& str1, const string& str2) {
-		if (str1.length() == str2.length()) return 60;
+		if (isSameLength(str1, str2)) return 60;
+		if (isOverTwiceSize(str1, str2))
+			return 0;
+		return -1;
+	}
+	bool isOverTwiceSize(const std::string& str1, const std::string& str2)
+	{
 		if (str1.length() >= str2.length()) {
 			if (str1.length() >= (str2.length() * 2))
-				return 0;
-			else
-				return -1;
+				return true;
 		}
 		else {
 			if (str2.length() >= (str1.length() * 2))
-				return 0;
-			else
-				return -1;
+				return true;
 		}
-		return -1;
+		return false;
+	}
+	bool isSameLength(const std::string& str1, const std::string& str2)
+	{
+		return str1.length() == str2.length();
 	}
 };
